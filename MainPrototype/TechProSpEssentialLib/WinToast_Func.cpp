38e8bc4479cfa2fc02451812be6a34756b273325
Janrey Licas <janjan.licas@gmail.com>
2019-08-16 17:00:34 +0800

    [Repo Cleanup Finalization] Completed Cleanup.
    NOTE: All files have been cleared that doesn't needed to be in repo. Continuation of Individual Change Commitment will be start to continue.

---
+++ MainPrototype/TechProSpEssentialLib/WinToast_Func.cpp
@@ -0,0 +1,42 @@
+#include "TechProSpEssentialLib/TechProSpEssential.h"
+
+void TTRM::TTRM_TechFunc::toastActivated() const
+{
+    std::wcout << L"The user clicked in this toast" << std::endl;
+    exit(0);
+}
+
+void TTRM::TTRM_TechFunc::toastActivated(int actionIndex) const
+{
+    std::wcout << L"The user clicked on action #" << actionIndex << std::endl;
+    exit(16 + actionIndex);
+}
+
+void TTRM::TTRM_TechFunc::toastDismissed(WinToastDismissalReason state) const
+{
+    switch (state)
+    {
+    case UserCanceled:
+        std::wcout << L"The user dismissed this toast" << std::endl;
+        exit(1);
+        break;
+    case TimedOut:
+        std::wcout << L"The toast has timed out" << std::endl;
+        exit(2);
+        break;
+    case ApplicationHidden:
+        std::wcout << L"The application hid the toast using ToastNotifier.hide()" << std::endl;
+        exit(3);
+        break;
+    default:
+        std::wcout << L"Toast not activated" << std::endl;
+        exit(4);
+        break;
+    }
+}
+
+void TTRM::TTRM_TechFunc::toastFailed() const
+{
+    std::wcout << L"Error showing current toast" << std::endl;
+    exit(5);
+}
